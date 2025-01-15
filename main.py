c50009eb608acaef693f70afeef3c1ed6376e1f2
FUYU <zczlyf7@ucl.ac.uk>
2025-01-15 09:57:23 +0000

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-
+# 12
 if __name__ == "__main__":
     # Get the path to A.py
     a_script_path = os.path.join(os.path.dirname(__file__), "A", "A_logistic_regression_combined_dataset.py")
